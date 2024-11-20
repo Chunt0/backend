cba6f6543c50d2d4780a922d37a2c416745b39df
Chunt0 <chunt.mail@gmail.com>
2024-11-20 11:12:37 -0800

    2024-11-20 11:12:37

--- src/verification.rs
+++ src/verification.rs
@@ -7,7 +7,7 @@ pub async fn verify_user_credentials(
     pool: &PgPool,
 ) -> Result<Option<i32>, sqlx::Error> {
     let result = sqlx::query!(
-        "SELECT token_amount FROM users WHERE public_key = $1",
+        "SELECT token_amount FROM accounts WHERE public_key = $1",
         public_key
     )
     .fetch_optional(pool)
@@ -20,7 +20,7 @@ pub async fn verify_user_credentials(
 pub async fn add_user(public_key: &str, pool: &PgPool) -> Result<i32, sqlx::Error> {
     let initial_token_amount = 0;
     sqlx::query!(
-        "INSERT INTO users (public_key, token_amount) VALUES ($1, $2)",
+        "INSERT INTO accounts (public_key, token_amount) VALUES ($1, $2)",
         public_key,
         initial_token_amount
     )
